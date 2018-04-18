89f72d913617fe11b9bf8bf3b467368cb6a1f966
vinothsundararajan <vinothsundararajan@outlook.com>
2018-04-18 18:15:51 +0530

    MULTIPLE NUMBER OF ARUG USING VIA .FORMAT

--- functions.py
+++ functions.py
@@ -5,13 +5,15 @@ s1 = int(argv[1])
 s2 = int(argv[2])
 # function
 def hello(x,y):
-    print ("Hello world")
+    #print ("Hello world")
     sum = x + y
     sub = x - y
     mul = x * y
     div = x / y
     mol = x % y
-    print (sum,sub,mul,div,mol)
+    #print (sum,sub,mul,div,mol)
+    print ("The sum of two number is {0} \nThe subtractor of two number is {1} \nThe multiple of two numbers is {2} \
+    \nThe division of two number is {3} \nThe modules of two number is {4}".format(sum,sub,mul,div,mol))
 #main program
 if s1 is None:
     print ("Missing input params s1")
