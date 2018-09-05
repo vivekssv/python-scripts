452c701b58ef3cd25fb454908a48398ed8cc9870
Vinoth_Sundararajan <vinothsundararajan@outlook.com>
2018-09-05 08:49:06 +0530

    VARIABLES STRUCTURE IN PYTHON

---
+++ variables_structure.py
@@ -0,0 +1,6 @@
+my_string = 'Hello, World!'
+my_flt = 45.06
+my_bool = 5 > 9 #A Boolean value will return either True or False
+my_list = ['item_1', 'item_2', 'item_3', 'item_4']
+my_tuple = ('one', 'two', 'three')
+my_dict = {'letter': 'g', 'number': 'seven', 'symbol': '&'}
