b4732d60e4d09ae456f46717ab6d56be7284f8c3
Vinoth Sundararajan <Vinoth Sundararajan@tsi.com>
2018-09-10 17:36:04 +0530

    DICTIONARY TO HANDLE JSON AS INPUT

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 import sys
+import json
+json_data = """ {
+    "text key": "string",
+    "integer key": 123,
+    "boolean key": true,
+    "simple list": [
+        "value 1",
+        "value 2"
+    ],
+    "dict": {
+        "element 1": "value E.1",
+        "element 2": "value E.2"
+    },
+    "dict list": [
+        {
+            "key 1.1": "value 1.1",
+            "key 1.2": "value 1.2"
+        },
+        {
+            "key 2.1": "value 2.1",
+            "key 2.2": "value 2.2"
+        }
+    ]
+} """
+my_dictionary = json.loads(json_data)
+print(my_dictionary, indent=4)
 
 STUDENTS={
     'VINOTH' : ['90','80','70'],
