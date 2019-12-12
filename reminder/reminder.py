6867cdfa08f2caafb793d010cc1f9b9dab39e2a6
vinothsundararajan@outlook.com <vinoth@TPFLAP164.tsi.com>
2019-12-12 08:22:35 +0530

    BIRTHDAY REMINDER WORKING WITH TOASTER 2.0 IMPROVED

--- reminder/reminder.py
+++ reminder/reminder.py
@@ -6,21 +6,30 @@ from win10toast import ToastNotifier
 toaster = ToastNotifier()
 
 # For Windows:
-birthdayFile = 'C:\\Vinoth\\Python\\python-scripts\\reminder\\reminder.py'
+birthdayFile = 'C:\\Vinoth\\Python\\python-scripts\\reminder\\reminderfile.txt'
 # For Linux:
-#birthdayFile = '/mnt/python-scripts/reminder/reminder.py'
+#birthdayFile = '/mnt/python-scripts/reminder/reminderfile.txt'
 #
 def checkTodaysBirthdays():
     fileName = open(birthdayFile, 'r')
     today = time.strftime('%m%d')
     flag = 0
     for line in fileName:
+#       print (line.split(' '))
         if today in line:
             flag = 1
-            print (line[1])
-            line = line.spilt(' ')
-            toaster.show_toast("Birthday Reminder Today", "This notification + line[1] + ' ' + line[2] + ' ' ", icon_path = "custom.ico", duration = 5, threaded = True)
+            line = line.split(' ')
+            #print (line[2] + ' ' + line[1])
+            toaster.show_toast("BIRTHDAY REMINDER TODAY",
+                               'This is reminder to wish'" " + line[1] + " " + line[2] + 'as his birthday today',
+                               duration = 5,
+                               icon_path = "reminder.ico",
+                               threaded = True)
     if flag == 0:
-        toaster.show_toast("No Birthday Today", "Kindly Ignore this note", icon_path = "custom.ico", duration = 10, threaded = True)
+        toaster.show_toast("No Birthday Today",
+                           "Kindly Ignore this note",
+                           icon_path = "reminder.ico",
+                           duration = 10,
+                           threaded = True)
 if __name__ == '__main__':
     checkTodaysBirthdays()
